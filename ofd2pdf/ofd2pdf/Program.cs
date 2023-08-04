811187ad149b707c82580d378c7a44b85cbb7def
Merrick Zhang <anphorea@gmail.com>
2023-08-04 14:01:17 +0800

    init

---
+++ ofd2pdf/ofd2pdf/Program.cs
@@ -0,0 +1,32 @@
+﻿using Spire.Pdf.Conversion;
+using System;
+
+namespace ofd2pdf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("OFD TO PDF 转换工具");
+                Console.WriteLine("使用方式： ofd2pdf.exe <ofdfile>");
+            }
+            else
+            {
+                var ofdfile = args[0]; 
+                if (ofdfile.EndsWith("ofd",true,null))
+                {
+                    Console.WriteLine($"#转换 {ofdfile}");
+                    OfdConverter oc = new OfdConverter(ofdfile);
+                    oc.ToPdf(ofdfile+".pdf");
+                    Console.WriteLine($"#完成 {ofdfile}");
+                }
+                else
+                {
+                    Console.WriteLine($"{ofdfile} 不是OFD文件");
+                }
+            }
+        }
+    }
+}
