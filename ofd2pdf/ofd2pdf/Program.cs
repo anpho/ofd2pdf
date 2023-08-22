b2fb7997416c6bcbbdc5b06ec8c265f78a151ed1
MerrickZ <anphorea@gmail.com>
2023-08-22 19:03:21 +0800

    license

--- ofd2pdf/ofd2pdf/Program.cs
+++ ofd2pdf/ofd2pdf/Program.cs
@@ -8,6 +8,20 @@ namespace ofd2pdf
     {
         static void Main(string[] args)
         {
+            /*
+             * Spire.PDF 的 License 部分，此前使用的是个人临时License，需要自行替换
+             */
+            string currentFolder=System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+            string licenseFile = System.IO.Path.Combine(currentFolder, "license.elic.xml");
+            if (System.IO.File.Exists(licenseFile) ) {
+                Spire.Pdf.License.LicenseProvider.SetLicenseFileName(licenseFile);
+            }
+            else
+            {
+                Console.WriteLine("Spire.PDF License 文件(license.elic.xml)未找到");
+                MessageBox.Show("Spire.PDF License 文件(license.elic.xml)未找到");
+            }
+            
             if (args.Length == 0)
             {
                 Console.WriteLine("OFD TO PDF 转换工具");
@@ -24,7 +38,6 @@ namespace ofd2pdf
                     string newFilename = ofdfile.Replace(".ofd", ".pdf");
                     oc.ToPdf(newFilename);
                     Console.WriteLine($"#完成 {ofdfile}");
-
                 }
                 else
                 {
